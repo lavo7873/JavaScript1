064333b0668818646bc85327815fa3fa6f01f1f6
lavo7873 <linhuang1101@gmail.com>
2022-07-22 20:58:55 -0800

    update

---
+++ console/style2.js
@@ -0,0 +1,12 @@
+let f = 20.5
+let i = 10
+let b = true
+let s = "Hà Nội"
+
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
