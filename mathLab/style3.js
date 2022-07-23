734171dd3b861798ca9db9e5a602eb3966bee991
lavo7873 <linhuang1101@gmail.com>
2022-07-22 21:15:33 -0800

    update

---
+++ mathLab/style3.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter the Width : ");
+inputHeight = prompt("Enter the Height : ");
+
+let Width = parseInt(inputWidth);
+let Height = parseInt(inputHeight);
+
+let area = Width * Height;
+document.write("The Area is : " + area);
